b992f839794d5165df9874fd647d0adf14c9ec92
manmohanbcs11 <manmohanbcs11@gmail.com>
2024-03-05 20:54:04 +0530

    setting the project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './App.css';
+import { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>Hello start with class based component</div>
+    )
+  }
+}
