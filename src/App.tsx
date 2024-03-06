ecf23ba4056ed57d3e18dc131f0b0c97fc3ff8e2
manmohanbcs11 <manmohanbcs11@gmail.com>
2024-03-06 16:20:29 +0530

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import { Component } from 'react';
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
-      <div>Hello start with class based component</div>
+      <div>
+        <NavBar></NavBar>
+        <News></News>
+      </div>
     )
   }
 }
---
+++ src/components/News.tsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>
+        News Component
+        <NewsItem></NewsItem>
+        <NewsItem></NewsItem>
+        <NewsItem></NewsItem>
+        <NewsItem></NewsItem>
+        <NewsItem></NewsItem>
+      </div>
+    )
+  }
+}
+
+export default News
